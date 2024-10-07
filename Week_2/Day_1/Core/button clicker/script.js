1749a97b333393ffcd7dbb82378d7dc9a0cb4477
Mohamed Aziz Tounsi <tounsiasiz@gmail.com>
2024-10-07 15:37:07 +0100

    clicked button

---
+++ Week_2/Day_1/Core/button clicker/script.js
@@ -0,0 +1,13 @@
+function logout(element) {
+
+    element.innerText = "logout";
+}
+function remove(element){
+    element.remove();
+}
+function like(element){
+    alert("ninja was liked")
+}
+
+
+
